b8b0ce53004231999b05a7570e7d3adea9a7c763
JungAh <70677614+Jungahgo@users.noreply.github.com>
2023-07-06 15:39:54 +0900

    ast.h 수정

--- ast.h
+++ ast.h
@@ -5,6 +5,7 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <stdbool.h>
+#include <unistd.h>
 
 # define CMD 0
 # define PLINE 1
@@ -46,6 +47,40 @@ typedef struct s_pline
 	t_cmd	*next;
 }	t_pline;
 
+char		**ft_split(const char *str, char c);
+char		*ft_strjoin(char *s1, char *s2);
+char		*ft_strnstr(const char *haystack, const char *needle, size_t len);
+size_t		ft_strlen(const char *s);
+char		*ft_strdup_size(char *s1, int limit_size, int start);
+char		*ft_strdup(const char *s1);
+
+char 		**find_path(char **envp);
+char 		*find_file(char **path_list, char *command);
+char 		**get_command_list(t_cmd *cmd);
+void		free_char_list(char **list);
+void		print_char_list(char **list);
+pid_t 		cmd_exe(void *list, char **envp);
+
+t_word	*new_word(char	*text);
+t_redi	*new_redi(char *op, char *file);
+t_suff	*new_suff(int type, char *text, char *op, char *file);
+t_cmd	*new_cmd(void);
+
+void	perror_n_exit(char *s, int status);
+int		is_pipe_included(char *in);
+
+void	*parsing_input(int type, char **in, char **env);
+t_cmd	*make_cmd(char **in, char **env);
+void	make_suffix(t_cmd *cmd, int idx, char **in);
+
+t_suff	*suffix_in_quote(t_cmd *cmd, char *in);
+void	remove_quote(char *s);
+
+int		is_sep(char n);
+int		count_word(char *s);
+int		get_word_len(char *s, int *idx);
+char	*init_word(char *s, int *idx);
+char	**split_input(char *s);
 #endif
 /*
 
